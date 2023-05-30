fee590c5d59aa7fe08131fee012bdd96d5c64bdf
plamyx <albanese.aurelien06@gmail.com>
2023-05-30 13:57:16 +0200

    db update

--- src/database.js
+++ src/database.js
@@ -7,6 +7,7 @@ const db = knex({
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
